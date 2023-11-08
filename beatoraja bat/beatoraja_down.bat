d9a00283c454348abecd63e052d7f72309689616
kuroling <110186517+kuroling@users.noreply.github.com>
2023-11-09 01:44:28 +0900

    Add files via upload
    beatoraja 0.8.6 + openjdk21 + javafx down

---
+++ beatoraja bat/beatoraja_down.bat
@@ -0,0 +1,22 @@
+curl "https://raw.githubusercontent.com/kuroling/kor_beatoraja_wiki/main/beatoraja%20bat/beatoraja-openjdk.bat" --output "beatoraja-openjdk.bat"
+curl "https://mocha-repository.info/download/beatoraja0.8.6-modernchic.zip" --output "beatoraja0.8.6-modernchic.zip"
+curl -LO  "https://corretto.aws/downloads/latest/amazon-corretto-21-x64-windows-jdk.zip" --output "jdk.zip"
+curl "https://download2.gluonhq.com/openjfx/21.0.1/openjfx-21.0.1_windows-x64_bin-sdk.zip" --output "javafx.zip"
+powershell Expand-Archive 'beatoraja0.8.6-modernchic.zip' -DestinationPath 'beatoraja/'
+powershell Expand-Archive 'amazon-corretto-21-x64-windows-jdk.zip' -DestinationPath 'jdk/'
+powershell Expand-Archive 'javafx.zip' -DestinationPath 'javafx/'
+powershell Remove-Item -Force .\javafx.zip
+powershell Remove-Item -Force .\amazon-corretto-21-x64-windows-jdk.zip
+powershell Remove-Item -Force .\beatoraja0.8.6-modernchic.zip
+cd .\beatoraja\beatoraja*
+mkdir javafx-sdk
+mkdir jdk
+cd ..\..\
+cd .\javafx\javafx*
+powershell Move-Item .\* ..\..\beatoraja\beatoraja*\javafx-sdk
+cd ..\..\
+cd .\jdk\jdk*
+powershell Move-Item .\* ..\..\beatoraja\beatoraja*\jdk
+cd ..\..\
+powershell Remove-Item -Force -Recurse .\javafx
+powershell Remove-Item -Force -Recurse .\jdk
